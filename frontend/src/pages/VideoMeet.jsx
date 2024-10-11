37f6d2709468cb1a051370969fae1b9b16c3b687
Ashitosh Katale <kataleashitosh123@gmail.com>
2024-10-11 23:22:57 +0530

    Vercel Deploy

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -12,7 +12,7 @@ import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
 import server from '../../../backend/src/environment';
-// import "./VideoMeetStyle.css";
+import "./VideoMeetStyle.css";
 
 const server_url = `${server}`;
 
